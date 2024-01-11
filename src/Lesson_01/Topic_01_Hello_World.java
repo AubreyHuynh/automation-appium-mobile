85657533a5039624a77b40c7eac1c63db04e37a6
HuynhMinhNghia <huynhminhnghia.hmn99@gmail.com>
2024-01-11 08:27:01 +0700

    Initial commit

---
+++ src/Lesson_01/Topic_01_Hello_World.java
@@ -0,0 +1,13 @@
+package Lesson_01;
+
+public class Topic_01_Hello_World {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello world!");
+    }
+
+    // Readability (Dễ đọc, hiểu)
+    // Maintenance (Dễ bảo trì) <-- DRY principle, Single Responsibility
+    // Extendable
+}
---
+++ src/Lesson_01/Topic_02_Variable.java
@@ -0,0 +1,39 @@
+package Lesson_01;
+
+public class Topic_02_Variable {
+
+    public static void main(String[] args) {
+        // naming convention: camelCase, snake_case, PascalCase
+        int myInt = 19;
+        // Re-assign value
+        myInt = 18;
+        // Re-declaration is NOT allowed
+        // int myInt = 20;
+
+        int my_num = 18;
+        int MyValue = 10;
+
+        long myLongNum = 10L;
+        float myFloatNum = 3.14f;
+        double myDoubleNum = 3.14d;
+
+        boolean myDecision01 = true;
+        boolean myDecision02 = false;
+
+        // byte, short, int, long
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Long.MAX_VALUE);
+
+        // incompatible types: possible lossy conversion from int to byte
+        // byte myByte = 326;
+
+        // Character
+        final float PI_NUMBER = 3.14f;
+        // cannot re-assign final variable
+        // PI_NUMBER = 3.15f;
+
+
+    }
+}
